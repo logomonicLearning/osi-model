31a88b6dfc2c497c207dd777dc8c09c13ba080b7
Logomonic <logomoniclearningmail.com>
2020-10-06 15:39:50 +0100

    first commit

---
+++ Archive/osiConcepts.js
@@ -0,0 +1,33 @@
+const conceptArr=[]
+
+
+conceptArr.push()
+ 
+	'Https', 
+	'tftp', 
+	'sftp', 
+	'smtp', 
+	'pop3', 
+	'dhcp', 
+	'dns', 
+	'Ascii', 
+ 	'utf-8', 
+ 	'html', 
+ 	'sql', 
+ 	'php', 
+ 	'js', 
+ 	'css', 
+ 	'bin', 
+ 	'hex',
+	'ebcdic', 
+	'ssh', 
+	'X.225' 
+	'tcp', 
+	'udp', 
+	'ssl/tls', 
+ Network
+
+ Datalink
+ Physical
+
+
